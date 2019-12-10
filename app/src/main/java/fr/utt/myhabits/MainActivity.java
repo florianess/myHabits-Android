2e5a87b7383120e2acf3fffe099c8006fae0451c
Florian Esslinger <florian.esslinger@live.fr>
2019-12-10 16:37:21 +0100

    Initial commit

---
+++ app/src/main/java/fr/utt/myhabits/MainActivity.java
@@ -0,0 +1,52 @@
+package fr.utt.myhabits;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import fr.utt.myhabits.ui.home.HomeFragment;
+import fr.utt.myhabits.ui.stats.StatsFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavItem);
+            showFragment(new HomeFragment());
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+            .replace(R.id.content, fragment)
+                .addToBackStack(null)
+            .commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavItem
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    showFragment(new HomeFragment());
+                    return true;
+                case R.id.navigation_stats:
+                    showFragment(new StatsFragment());
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+}
---
+++ app/src/main/java/fr/utt/myhabits/ui/home/HomeFragment.java
@@ -0,0 +1,57 @@
+package fr.utt.myhabits.ui.home;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import fr.utt.myhabits.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        final FloatingActionButton fab = root.findViewById(R.id.floating_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fab.setExpanded(!fab.isExpanded());
+            }
+        });
+        MaterialButton addButton = root.findViewById(R.id.button_add);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fab.setExpanded(!fab.isExpanded());
+            }
+        });
+        return root;
+    }
+}
